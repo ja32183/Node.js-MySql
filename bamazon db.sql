5f2fd92cf183b2d5f521b4d33396f50f50ea6fe9
Jeff Amlani <ja32183@gmail.com>
2019-03-18 15:26:42 -0400

    added sql seed file

---
+++ bamazon db.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: select * from bamazon.products
+-- Date: 2019-03-18 15:19
+*/
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (1,'product1','department1',1,1);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (2,'product2','department2',2,2);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (3,'product3','department3',3,3);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (4,'product4','department4',4,4);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (5,'product5','department5',5,5);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (6,'product6','department6',6,6);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (7,'product7','department7',7,7);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (8,'product8','department8',8,8);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (9,'product9','department9',9,9);
+INSERT INTO `` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (10,'product10','department10',10,10);
