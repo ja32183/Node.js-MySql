488d0855fb4c95196525394411c31e119beebb18
Jeff Amlani <ja32183@gmail.com>
2019-03-26 12:54:33 -0400

    Added functions for displaying and calculating prices and inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,25 +14,117 @@ var connection = mysql.createConnection({
   password: "P@ssw0rd1!",
   database: "bamazon"
 });
-
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  afterConnection();
 });
-function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    });}
-  
-  function runSearch() {
-    inquirer
-    .prompt({
-        name: "action",
-        type: "rawlist",
-        message: "What is the ID of the item you would like to buy?",
-        choices: [
-    "Enter an ID number between 1 and 10",
-     ]})}
+
+function purchaseprompt() {
+  inquirer
+  .prompt([{
+      name: "item_id",
+      type: "input",
+      message: "What is the ID of the item you would like to buy?",
+      filter: Number},
+
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many do you want to buy",
+        filter: Number
+      }
+
+    ])
+.then(function(input)
+
+{
+var item = input.item_id;
+var quantity = input.quantity;
+var querydb = "Select * from products where ?";
+
+connection.query(querydb, {item_id: item}, function(err, data) {
+if (err) throw err;
+if (data.length===0){
+  console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+  displayInventory();
+
+}
+else {
+  var productData = data[0];
+
+
+				// console.log('productData = ' + JSON.stringify(productData));
+				// console.log('productData.stock_quantity = ' + productData.stock_quantity);
+
+				// If the quantity requested by the user is in stock
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+					// Construct the updating query string
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+					// console.log('updateQueryStr = ' + updateQueryStr);
+
+					// Update the inventory
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your order has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n---------------------------------------------------------------------\n");
+
+						// End the database connection
+						connection.end();
+})}
+else {
+  console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+  console.log('Please modify your order.');
+  console.log("\n---------------------------------------------------------------------\n");
+
+  displayInventory();
+}
+}
+})
+})
+}
+
+function displayInventory() {
+	// console.log('___ENTER displayInventory___');
+
+	// Construct the db query string
+	queryStr = 'SELECT * FROM products';
+
+	// Make the db query
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		var strOut = '';
+		for (var i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+			strOut += 'Department: ' + data[i].department_name + '  //  ';
+			strOut += 'Price: $' + data[i].price + '\n';
+
+			console.log(strOut);
+    }
+    
+    console.log("---------------------------------------------------------------------\n");
+
+	  	//Prompt the user for item/quantity they would like to purchase
+	  	purchaseprompt();
+	})
+}
+
+// runBamazon will execute the main application logic
+function runBamazon() {
+	// console.log('___ENTER runBamazon___');
+
+	// Display the available inventory
+	displayInventory();
+}
+
+// Run the application logic
+runBamazon();
